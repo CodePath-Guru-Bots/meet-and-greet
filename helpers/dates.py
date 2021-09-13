959413dc4a512844104793dd62bc2838e9cb1748
Guillermo <Sanguillermo98@gmail.com>
2021-09-13 17:52:33 -0400

    Changed it to daily

--- helpers/dates.py
+++ helpers/dates.py
@@ -8,7 +8,8 @@ def get_rotation(initial_date: date) -> int:
   Gets the amount of weeks that has passed since `initial_date` using the start date.
   """
   current_date = date.today()
-  weeks_since = abs(current_date - initial_date).days // 7
+  # weeks_since = abs(current_date - initial_date).days // 7
+  weeks_since = abs(current_date - initial_date).days
   return weeks_since
 
 
