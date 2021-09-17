258b79f2b59173edccb59e8d83240c1034e8fb85
Guillermo <Sanguillermo98@gmail.com>
2021-09-16 22:40:48 -0400

    Algorithm changes + how it works documentation

---
+++ how_it_works.md
@@ -0,0 +1,70 @@
+Knowledge Check
+
+Figure out to make sure that each week we get a unique pair of students. 
+Before learning the content below, do some research on:
+
+- Intro to Combinations
+- Combinations vs Permutations
+
+Everything else I just googled my way through to understand and find the code to 
+create unique pairs each week (examples below)
+
+
+Practice creating tests of combinations with students ABCD and then ABCDEF.
+Go through your head on how to create the combinations of 2 students.
+Next step is to figure out to create pairs of combinations, example:
+
+   Week 1 pairings --> AB CD
+
+ABCD
+
+Using Combinations (python itertools <-- google this)
+
+4 total people
+6 combinations total
+2 pairs per week
+
+
+AB AC AD | BC BD | CD
+1  2  3    4  5    6
+
+
+1 AB CD     1, 6
+2 AC BD     2, 5
+3 AD BC     3, 4
+
+[{('A', 'B'), ('C', 'D')}, 
+{('A', 'C'), ('B', 'D')}, 
+{('A', 'D'), ('B', 'C')}]
+
+
+-------
+
+
+ABCDEF
+6 people total
+15 combinations total
+3 pairs per week (6/2)
+
+
+AB AC AD AE AF | BC BD BE BF | CD CE CF | DE DF | EF
+1  2  3  4  5     6  7  8 9    10 11 12   13 14   15
+
+
+1 AB CD EF        1, 10, 15 
+2 AC BE DF        2, 8, 14
+3 AD BC DE        3, 6, 13
+4 AE BD CF        4, 7, 12
+5 AF BF CE        5, 9, 11
+
+
+I figured it out by this point through this link:
+https://stackoverflow.com/questions/54200347/python-get-groups-of-combinations-that-each-member-appear-only-once
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
 
+# Python Libraries
+import os
 from datetime import date
+from itertools import combinations, chain
+
+# Third-party
 from slack_sdk import WebClient
 
 # Helper functions
@@ -12,45 +16,58 @@ from helpers.conversations import Conversations
 channel = os.environ['CHANNEL']
 client = WebClient(token=os.environ['BOT_TOKEN'])
 
-
-def rotate(array: [str], rotation: int) -> [str]:
-  """
-  Rotate users so that we prevent them from meeting with the same people again.
-  We accomplish this by shifting the first student to the end of the line.
-  """
-  # Rotate meetups (i.e. shift student 'John' to the end of array)
-  index = rotation % len(array)
-  array.append(array.pop(index))
-  return array
-
 def start_conversation(user_pair: (str, str)):
   """
-  Start conversation with a pair of users (pair = 2 users) + bot user.
+  Start conversation with a pair of users (pair = 2 users)
   Bot sends a welcome message to motivate students to introduce each other.
   """
   conversation = Conversations(client=client, user_pair=user_pair)
   conversation.start()
 
-def create_user_pairs(array: [str]) -> [[str, str]]:
-    return [ [array[x], array[-x - 1]] for x in range(len(array) // 2) ]
+def create_user_pairs(user_array: [str], rotation: int) -> [[str, str]]:
+  rotation_count = rotation % (len(user_array) - 1)
+  
+  c = combinations(user_array, 2)
+  pair_quantity = len(user_array) // 2
+  if len(user_array) % 2 != 0:
+    pair_quantity += 1
 
-def create_conversations(user_pairs: [[str, str]]):
+  # This big line basically creates a list of sets of pairs for each week
+  # This ensures that there are no repeats for that particular week
+  # Everyone gets a chance to meet everyone
+  unique_pairings = [list(set(i)) for i in list(combinations(c,pair_quantity)) if (len(set(user_array) & set(chain(*i))) == len(user_array))]
+  return unique_pairings[rotation_count]
+  
+
+def create_conversations(all_pairings: [[(str, str)]]):
   """
   Start conversation with user pairs of users that will meet up this week
   """
-  for user_pair in user_pairs:
+  for user_pair in all_pairings:
       start_conversation(user_pair=user_pair)
-
 
 def main():
   initial_date = date(2021,9,13)
   rotation = get_rotation(initial_date)
   all_users = get_users(channel)
-  rotated = rotate(all_users, rotation)
-  user_pairs = create_user_pairs(rotated)
+  user_pairs = create_user_pairs(all_users, rotation)
   create_conversations(user_pairs)
 
+# def test():
+#   users = list("ABCDEFG")
+#   create_user_pairs(users, 0)
+
+# def test():
+#   date1, date2, date3, date4, date5, date6 = date(2021,9,7), date(2021,9,8), date(2021,9,9), date(2021, 9,10), date(2021,9,11), date(2021,9,12)
+#   dates = [date1, date2, date3, date4, date5, date6]
+#   for d in dates:
+#     rotation = get_rotation(d)
+#     all_users = get_users(channel)
+#     user_pairs = create_user_pairs(all_users, rotation)
+#     # print(user_pairs)
+#     # create_conversations(user_pairs)
 
 if __name__ == "__main__":
   main()
+  # test()
 
